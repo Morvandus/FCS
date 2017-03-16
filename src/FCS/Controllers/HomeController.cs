b1448efb601c9fa77c628fa4dc5e597cfabbc406
Morvandus <zenoxks@gmail.com>
2017-03-16 11:31:00 +0200

    Testing exceptions

--- src/FCS/Controllers/HomeController.cs
+++ src/FCS/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
 
 namespace FCS.Controllers
 {
@@ -10,6 +11,7 @@ namespace FCS.Controllers
     {
         public IActionResult Index()
         {
+            throw new Exception();         
             return View();
         }
 
